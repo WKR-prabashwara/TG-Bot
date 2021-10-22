ebb9ffb26313e1a9ae0c75f159105badad397fed
Rivibibu Prabshwara <90378349+WKRPrabashwara@users.noreply.github.com>
2021-10-22 11:47:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,7 @@ bot = telebot.TeleBot("2055977464:AAFH43eSWHRWS5hPghaaehD6L9TSSpCfe6Y")
 def send_welcome(message):
     bot.reply_to(message,"Hᴇʟʟᴏ Tʜᴇʀᴇ👋, I ᴀᴍ [Sᴏᴘʜɪᴀ](https://t.me/The_Sophia_Bot) ʙᴏᴛ 🍑
 I ᴀᴍ ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ꜰᴇᴍᴀʟᴇ ʙᴏᴛ. Yᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴍᴇ ᴛᴏ ᴇᴀꜱɪʟʏ ᴀɴᴅ ꜱᴍᴏᴏᴛʜʟʏ ᴍᴀɴᴀɢᴇ ʏᴏᴜʀ ʜᴏᴍᴇ 🙋‍♀️
-Hɪᴛ /help ᴛᴏ ꜰɪɴᴅ ᴍʏ ʟɪꜱᴛ ᴏꜰ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴀɴᴅꜱ 🕹 
-                 buttons = [
-    [
-        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 💬", url="https://t.me/BoTupdateZone"),
-        InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ 👥", url="https://t.me/tgzoNechat"),
-    ],
-    [
-        InlineKeyboardButton(text="ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ 📜", url="https://github.com/WKRPrabashwara/Sophia_Bot"),
-        InlineKeyboardButton(text="ʜᴇʟᴘ ❔", url="http://t.me/The_Sophia_Bot?start=help"),
-    ],
-    [
-        InlineKeyboardButton(text="ᴅᴇᴠᴇʟᴏᴘᴇʀ 👨‍💻", url="https://t.me/ImshZe"),
-    ],
-    [
-        InlineKeyboardButton(
-            text="➕ ᴀᴅᴅ ᴀɴᴋɪ ᴠᴇᴄᴛᴏʀ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕", url="t.me/The_Sophia_Bot?startgroup=true"
-        ),
-    ],
-]")
+Hɪᴛ /help ᴛᴏ ꜰɪɴᴅ ᴍʏ ʟɪꜱᴛ ᴏꜰ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴀɴᴅꜱ 🕹")
                  
 
 
