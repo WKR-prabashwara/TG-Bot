a883e62f64169035bf4ab0e8ee18cd1136a87e4c
Rivibibu Prabshwara <90378349+WKRPrabashwara@users.noreply.github.com>
2021-10-22 11:16:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import telebot
+
+bot = telebot.TeleBot("API Key Here")
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.reply_to(message,"Hello There! I'm Prabshwara's Chat bot")
+
+@bot.message_handler(commands=["how"])
+def send_message(message):
+    bot.reply_message(message,"t.me/ImshZe")
+    
+bot.polling()
